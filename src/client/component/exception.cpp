533a007d4594468a453176ff1158484be3ccf7b2
quaK <38787176+Joelrau@users.noreply.github.com>
2021-02-25 10:03:54 +0200

    Minor fixes

--- src/client/component/exception.cpp
+++ src/client/component/exception.cpp
@@ -184,7 +184,7 @@ namespace exception
 		void write_minidump(const LPEXCEPTION_POINTERS exceptioninfo)
 		{
 			const std::string crash_name = utils::string::va("minidumps/s1x-crash-%d-%s.zip",
-			                                                 game::environment::get_mode(), get_timestamp().data());
+			                                                 game::environment::get_real_mode(), get_timestamp().data());
 
 			utils::compression::zip::archive zip_file{};
 			zip_file.add("crash.dmp", create_minidump(exceptioninfo));
