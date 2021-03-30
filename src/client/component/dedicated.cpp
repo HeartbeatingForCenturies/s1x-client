0e44cd1a636cf94380822bcd1fe331fc7e85aacf
Maurice Heumann <mauriceheumann@gmail.com>
2021-03-30 19:28:13 +0200

    Send heartbeat only every 10 minutes

--- src/client/component/dedicated.cpp
+++ src/client/component/dedicated.cpp
@@ -275,7 +275,7 @@ namespace dedicated
 
 				// Send heartbeat to dpmaster
 				scheduler::once(send_heartbeat, scheduler::pipeline::server);
-				scheduler::loop(send_heartbeat, scheduler::pipeline::server, 2min);
+				scheduler::loop(send_heartbeat, scheduler::pipeline::server, 10min);
 				command::add("heartbeat", send_heartbeat);
 			}, scheduler::pipeline::main, 1s);
 
