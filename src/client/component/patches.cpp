5e1b1538fb1a47a4bb8ec98301fbb3d1c68d0a13
Xerxes <5236639+xerxes-at@users.noreply.github.com>
2021-04-24 22:27:54 +0200

    Unlock sv_network_fps

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -301,6 +301,9 @@ namespace patches
 
 			// move chat position on the screen above menu splashes
 			dvars::override::Dvar_RegisterVector2("cg_hudChatPosition", 5, 170, 0, 640, game::DVAR_FLAG_SAVED);
+
+			// allow servers to check for new packages more often
+			dvars::override::Dvar_RegisterInt("sv_network_fps", 1000, 20, 1000, game::DVAR_FLAG_SAVED);
 		}
 
 		static void patch_sp()
