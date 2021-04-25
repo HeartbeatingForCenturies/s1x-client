342ec9e3555dbd44a9d1491aa25b55bd0a509c63
quaK <38787176+Joelrau@users.noreply.github.com>
2021-04-25 19:16:59 +0300

    Clean virtuallobby component

--- src/client/component/virtuallobby.cpp
+++ src/client/component/virtuallobby.cpp
@@ -9,67 +9,61 @@ namespace virtuallobby
 {
 	namespace
 	{
-		static game::dvar_t* virtualLobby_fov;
-		static game::dvar_t* virtualLobby_fovscale;
+		game::dvar_t* virtualLobby_fov;
+		game::dvar_t* virtualLobby_fovscale;
 
-		void* get_fov_stub()
+		const auto get_fov_stub = utils::hook::assemble([](utils::hook::assembler& a)
 		{
-			return utils::hook::assemble([](utils::hook::assembler& a)
-			{
-				const auto ret = a.newLabel();
-				const auto original = a.newLabel();
+			const auto ret = a.newLabel();
+			const auto original = a.newLabel();
 
-				a.pushad64();
-				a.call_aligned(game::VirtualLobby_Loaded);
-				a.cmp(al, 0);
-				a.je(original);
+			a.pushad64();
+			a.call_aligned(game::VirtualLobby_Loaded);
+			a.cmp(al, 0);
+			a.je(original);
 
-				// virtuallobby
-				a.popad64();
-				a.mov(rax, ptr(reinterpret_cast<int64_t>(&virtualLobby_fov)));
-				a.jmp(ret);
+			// virtuallobby
+			a.popad64();
+			a.mov(rax, ptr(reinterpret_cast<int64_t>(&virtualLobby_fov)));
+			a.jmp(ret);
 
-				// original
-				a.bind(original);
-				a.popad64();
-				a.mov(rax, qword_ptr(0x140BA7180));
-				a.jmp(ret);
+			// original
+			a.bind(original);
+			a.popad64();
+			a.mov(rax, qword_ptr(0x140BA7180));
+			a.jmp(ret);
 
-				a.bind(ret);
-				a.mov(ecx, dword_ptr(rsp, 0x78));
-				a.movss(xmm6, dword_ptr(rax, 0x10));
-				a.jmp(0x1401D5B0B);
-			});
-		}
+			a.bind(ret);
+			a.mov(ecx, dword_ptr(rsp, 0x78));
+			a.movss(xmm6, dword_ptr(rax, 0x10));
+			a.jmp(0x1401D5B0B);
+		});
 
-		void* get_fovscale_stub()
+		const auto get_fovscale_stub = utils::hook::assemble([](utils::hook::assembler& a)
 		{
-			return utils::hook::assemble([](utils::hook::assembler& a)
-			{
-				const auto ret = a.newLabel();
-				const auto original = a.newLabel();
+			const auto ret = a.newLabel();
+			const auto original = a.newLabel();
 
-				a.pushad64();
-				a.call_aligned(game::VirtualLobby_Loaded);
-				a.cmp(al, 0);
-				a.je(original);
+			a.pushad64();
+			a.call_aligned(game::VirtualLobby_Loaded);
+			a.cmp(al, 0);
+			a.je(original);
 
-				// virtuallobby
-				a.popad64();
-				a.mov(rax, ptr(reinterpret_cast<int64_t>(&virtualLobby_fovscale)));
-				a.jmp(ret);
+			// virtuallobby
+			a.popad64();
+			a.mov(rax, ptr(reinterpret_cast<int64_t>(&virtualLobby_fovscale)));
+			a.jmp(ret);
 
-				// original
-				a.bind(original);
-				a.popad64();
-				a.mov(rax, qword_ptr(0x140BA7188));
-				a.jmp(ret);
+			// original
+			a.bind(original);
+			a.popad64();
+			a.mov(rax, qword_ptr(0x140BA7188));
+			a.jmp(ret);
 
-				a.bind(ret);
-				a.lea(rcx, ptr(reinterpret_cast<int64_t>(*reinterpret_cast<void**>(0x1414C1700))));
-				a.jmp(0x1401D5CB8);
-			});
-		}
+			a.bind(ret);
+			a.mov(rcx, 0x1414C1700);
+			a.jmp(0x1401D5CB8);
+		});
 	}
 
 	class component final : public component_interface
@@ -81,10 +75,10 @@ namespace virtuallobby
 			virtualLobby_fovscale = game::Dvar_RegisterFloat("virtualLobby_fovScale", 1.0f, 0.0f, 2.0f, game::DVAR_FLAG_SAVED, "Field of view scaled for the virtual lobby");
 
 			utils::hook::nop(0x1401D5AFB, 16);
-			utils::hook::jump(0x1401D5AFB, get_fov_stub(), true);
+			utils::hook::jump(0x1401D5AFB, get_fov_stub, true);
 
 			utils::hook::nop(0x1401D5CAA, 14);
-			utils::hook::jump(0x1401D5CAA, get_fovscale_stub(), true);
+			utils::hook::jump(0x1401D5CAA, get_fovscale_stub, true);
 		}
 	};
 }
