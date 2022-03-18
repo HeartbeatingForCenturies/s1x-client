17caafece328b1b2ea1ef2d5d2dacbb2813a7e00
Skull Merlin <86374920+skkuull@users.noreply.github.com>
2022-03-18 22:16:34 +0200

    fix stats

--- src/client/component/stats.cpp
+++ src/client/component/stats.cpp
@@ -18,7 +18,7 @@ namespace stats
 		utils::hook::detour is_item_unlocked_hook2;
 		utils::hook::detour is_item_unlocked_hook3;
 
-		int is_item_unlocked_stub(void* a1, void* a2, void* a3)
+		int is_item_unlocked_stub(int a1, void* a2, int a3)
 		{
 			if (cg_unlock_all_items->current.enabled)
 			{
@@ -28,7 +28,7 @@ namespace stats
 			return is_item_unlocked_hook.invoke<int>(a1, a2, a3);
 		}
 
-		int is_item_unlocked_stub2(void* a1, void* a2, void* a3, void* a4, void* a5)
+		int is_item_unlocked_stub2(int a1, void* a2, void* a3, void* a4, int a5)
 		{
 			if (cg_unlock_all_items->current.enabled)
 			{
@@ -38,7 +38,7 @@ namespace stats
 			return is_item_unlocked_hook2.invoke<int>(a1, a2, a3, a4, a5);
 		}
 
-		int is_item_unlocked_stub3(void* a1)
+		int is_item_unlocked_stub3(int a1)
 		{
 			if (cg_unlock_all_items->current.enabled)
 			{
@@ -47,6 +47,11 @@ namespace stats
 
 			return is_item_unlocked_hook3.invoke<int>(a1);
 		}
+
+		int is_item_unlocked()
+		{
+			return 0; // 0 == yes
+		}
 	}
 
 	class component final : public component_interface
@@ -54,18 +59,28 @@ namespace stats
 	public:
 		void post_unpack() override
 		{
-			if (!game::environment::is_mp())
+			if (game::environment::is_sp())
 			{
 				return;
 			}
 
-			// unlock all items
-			cg_unlock_all_items = game::Dvar_RegisterBool("cg_unlockall_items", false, game::DVAR_FLAG_SAVED, "Whether items should be locked based on the player's stats or always unlocked.");
-			game::Dvar_RegisterBool("cg_unlockall_classes", false, game::DVAR_FLAG_SAVED, "Whether classes should be locked based on the player's stats or always unlocked.");
-
-			is_item_unlocked_hook.create(0x1403BD790, is_item_unlocked_stub); // LiveStorage_IsItemUnlockedFromTable_LocalClient
-			is_item_unlocked_hook2.create(0x1403BD290, is_item_unlocked_stub2); // LiveStorage_IsItemUnlockedFromTable
-			is_item_unlocked_hook3.create(0x1403BAF60, is_item_unlocked_stub3); // idk (unlocks loot etc)
+			if (game::environment::is_dedi())
+			{
+				// unlock all items
+				utils::hook::jump(0x1403BD790, is_item_unlocked); // LiveStorage_IsItemUnlockedFromTable_LocalClient
+				utils::hook::jump(0x1403BD290, is_item_unlocked); // LiveStorage_IsItemUnlockedFromTable
+				utils::hook::jump(0x1403BAF60, is_item_unlocked); // idk ( unlocks loot etc )
+			}
+			else
+			{
+				// unlock all items
+				cg_unlock_all_items = game::Dvar_RegisterBool("cg_unlockall_items", false, game::DVAR_FLAG_SAVED, "Whether items should be locked based on the player's stats or always unlocked.");
+				game::Dvar_RegisterBool("cg_unlockall_classes", false, game::DVAR_FLAG_SAVED, "Whether classes should be locked based on the player's stats or always unlocked.");
+			
+				is_item_unlocked_hook.create(0x1403BD790, is_item_unlocked_stub); // LiveStorage_IsItemUnlockedFromTable_LocalClient
+				is_item_unlocked_hook2.create(0x1403BD290, is_item_unlocked_stub2); // LiveStorage_IsItemUnlockedFromTable
+				is_item_unlocked_hook3.create(0x1403BAF60, is_item_unlocked_stub3); // idk (unlocks loot etc)
+			}
 
 			command::add("setPlayerDataInt", [](const command::params& params)
 			{
