16c7e3a9edc10f1fee9f072c819a63579173cf12
FutureRave <edoardo.sanguineti222@gmail.com>
2022-03-23 15:26:33 +0000

    Improve readability

--- src/client/component/dedicated_info.cpp
+++ src/client/component/dedicated_info.cpp
@@ -48,9 +48,9 @@ namespace dedicated_info
 					}
 				}
 
-				std::string cleaned_hostname(sv_hostname->current.string);
+				std::string cleaned_hostname = sv_hostname->current.string;
 
-				utils::string::strip(cleaned_hostname.data(), cleaned_hostname.data(),
+				utils::string::strip(sv_hostname->current.string, cleaned_hostname.data(),
 					cleaned_hostname.size() + 1);
 
 				console::set_title(utils::string::va("%s on %s [%d/%d] (%d)", cleaned_hostname.data(),
