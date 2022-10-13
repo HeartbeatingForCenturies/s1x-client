97fdc2389b35953c70bda5ff66defa1a0b1af816
FutureRave <edoardo.sanguineti222@gmail.com>
2022-10-13 22:25:54 +0100

    Fix logger

--- src/client/component/logger.cpp
+++ src/client/component/logger.cpp
@@ -1,8 +1,8 @@
 #include <std_include.hpp>
 #include "loader/component_loader.hpp"
+#include "game/game.hpp"
 
 #include "party.hpp"
-#include "game/game.hpp"
 #include "console.hpp"
 
 #include <utils/hook.hpp>
@@ -15,46 +15,38 @@ namespace logger
 
 		void print_error(const char* msg, ...)
 		{
-			char buffer[2048];
-
+			char buffer[2048]{};
 			va_list ap;
+
 			va_start(ap, msg);
-
-			vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
 			va_end(ap);
 
-			console::error(buffer);
+			console::error("%s", buffer);
 		}
 
 		void print_com_error(int, const char* msg, ...)
 		{
-			char buffer[2048];
-
+			char buffer[2048]{};
 			va_list ap;
+
 			va_start(ap, msg);
-
-			vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
 			va_end(ap);
 
-			console::error(buffer);
+			console::error("%s", buffer);
 		}
 
 		void com_error_stub(const int error, const char* msg, ...)
 		{
-			char buffer[2048];
+			char buffer[2048]{};
+			va_list ap;
 
-			{
-				va_list ap;
-				va_start(ap, msg);
+			va_start(ap, msg);
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
+			va_end(ap);
 
-				vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
-				va_end(ap);
-
-				console::error("Error: %s\n", buffer);
-			}
+			console::error("Error: %s\n", buffer);
 
 			party::clear_sv_motd(); // clear sv_motd on error if it exists
 
@@ -63,51 +55,44 @@ namespace logger
 
 		void print_warning(const char* msg, ...)
 		{
-			char buffer[2048];
-
+			char buffer[2048]{};
 			va_list ap;
+
 			va_start(ap, msg);
-
-			vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
 			va_end(ap);
 
-			console::warn(buffer);
+			console::warn("%s", buffer);
 		}
 
 		void print(const char* msg, ...)
 		{
-			char buffer[2048];
-
+			char buffer[2048]{};
 			va_list ap;
+
 			va_start(ap, msg);
-
-			vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
 			va_end(ap);
 
-			console::info(buffer);
+			console::info("%s", buffer);
 		}
 
 		void print_dev(const char* msg, ...)
 		{
-			static auto* enabled =
-				game::Dvar_RegisterBool("logger_dev", false, game::DVAR_FLAG_SAVED, "Print dev stuff");
-			if (!enabled->current.enabled)
+			static const auto* logger_dev = game::Dvar_RegisterBool("logger_dev", false, game::DVAR_FLAG_SAVED, "Print dev stuff");
+			if (!logger_dev->current.enabled)
 			{
 				return;
 			}
 
-			char buffer[2048];
-
+			char buffer[2048]{};
 			va_list ap;
+
 			va_start(ap, msg);
-
-			vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, msg, ap);
-
+			vsnprintf_s(buffer, _TRUNCATE, msg, ap);
 			va_end(ap);
 
-			console::info(buffer);
+			console::info("%s", buffer);
 		}
 
 		// nullsub_56
