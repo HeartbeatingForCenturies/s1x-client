9fa6602eb593c97a4d1581ebe493d4ca46bd3e3d
FutureRave <edoardo.sanguineti222@gmail.com>
2023-01-26 19:59:49 +0000

    feature: read bd files from data folder if they exists

--- src/client/game/demonware/services/bdStorage.hpp
+++ src/client/game/demonware/services/bdStorage.hpp
@@ -12,9 +12,9 @@ namespace demonware
 		using resource_variant = std::variant<std::string, callback>;
 		std::vector<std::pair<std::regex, resource_variant>> publisher_resources_;
 
-		void map_publisher_resource(const std::string& expression, INT id);
+		void map_publisher_resource(const std::string& expression, const std::string& path, int id);
 		void map_publisher_resource_variant(const std::string& expression, resource_variant resource);
-		bool load_publisher_resource(const std::string& name, std::string& buffer);
+		bool load_publisher_resource(const std::string& name, std::string& buffer) const;
 
 		void list_publisher_files(service_server* server, byte_buffer* buffer);
 		void get_publisher_file(service_server* server, byte_buffer* buffer);
